7f0e6120224eb4d3a1c614c89da523c2bb2d3191
David Tseng <inin2001@hotmail.com>
2013-12-13 18:26:27 +0800

    + init

---
+++ GotPet/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GotPet
+//
+//  Created by David Tseng on 12/13/13.
+//  Copyright (c) 2013 David Tseng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GotPet/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GotPet
+//
+//  Created by David Tseng on 12/13/13.
+//  Copyright (c) 2013 David Tseng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
